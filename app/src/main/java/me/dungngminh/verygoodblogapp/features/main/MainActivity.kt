74afe62f914bde698d5eedc58d59b1646c069191
dungngminhh <63831488+dungngminh@users.noreply.github.com>
2022-09-27 08:43:34 +0700

    feat(main_navigation): add main navigation

--- app/src/main/java/me/dungngminh/verygoodblogapp/features/main/MainActivity.kt
+++ app/src/main/java/me/dungngminh/verygoodblogapp/features/main/MainActivity.kt
@@ -1,7 +1,20 @@
 package me.dungngminh.verygoodblogapp.features.main
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import me.dungngminh.verygoodblogapp.R
 import me.dungngminh.verygoodblogapp.core.BaseActivity
 import me.dungngminh.verygoodblogapp.databinding.ActivityMainBinding
@@ -9,45 +22,21 @@ import me.dungngminh.verygoodblogapp.features.main.bookmark.BookmarkFragment
 import me.dungngminh.verygoodblogapp.features.main.home.HomeFragment
 import me.dungngminh.verygoodblogapp.features.main.notification.NotificationFragment
 import me.dungngminh.verygoodblogapp.features.main.user.UserFragment
+import timber.log.Timber
 
-class MainActivity : BaseActivity(R.layout.activity_main){
-
-
-    private var _binding: ActivityMainBinding? = null
-
-    private val binding get() = _binding!!
+class MainActivity : BaseActivity(R.layout.activity_main) {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+        navController = navHostFragment.navController
 
-        val homeFragment = HomeFragment()
-        val notificationFragment = NotificationFragment()
-        val bookmarkFragment = BookmarkFragment()
-        val userFragment = UserFragment()
-
-        setCurrentFragment(homeFragment)
-
-        binding.navView.setOnItemSelectedListener { item->
-            when(item.itemId){
-                R.id.itemHomePage -> setCurrentFragment(homeFragment)
-                R.id.itemNotification -> setCurrentFragment(notificationFragment)
-                R.id.itemBookmark -> setCurrentFragment(bookmarkFragment)
-                R.id.itemUser -> setCurrentFragment(userFragment)
-            }
-            true
-        }
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.setupWithNavController(navController)
     }
 
-    private fun setCurrentFragment(fragment: Fragment)=
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.nav_host_fragment,fragment)
-            commit()
-        }
-
-    override fun onDestroy() {
-        _binding = null
-        super.onDestroy()
-    }
 }
